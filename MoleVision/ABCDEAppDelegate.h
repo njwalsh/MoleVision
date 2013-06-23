5ae929d1b88e3c5676eb9fb21999293b40a26b95
njwalsh <nickjameswalsh1@gmail.com>
2013-06-23 15:46:24 -0700

    commit

---
+++ MoleVision/ABCDEAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ABCDEAppDelegate.h
+//  ABCDE Tutorial
+//
+//  Created by Adrienne C on 2013-06-21.
+//  Copyright (c) 2013 Adrienne C. All rights reserved.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABCDEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MoleVision/ABCDEViewController.h
@@ -0,0 +1,21 @@
+//
+//  ABCDEViewController.h
+//  ABCDE Tutorial
+//
+//  Created by Adrienne C on 2013-06-21.
+//  Copyright (c) 2013 Adrienne C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABCDEViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIView *label1a;
+@property (strong, nonatomic) IBOutlet UIView *label1b;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *label3;
+@property (weak, nonatomic) IBOutlet UILabel *label4;
+@property (weak, nonatomic) IBOutlet UILabel *label5;
+@property (weak, nonatomic) IBOutlet UILabel *label6;
+@property (weak, nonatomic) IBOutlet UILabel *label7;
+
+@end
