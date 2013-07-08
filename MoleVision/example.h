dc27a35650c3e3f93fbb0d533a15597e38c4b5ad
ivydong <idong@sfu.ca>
2013-07-08 16:18:10 -0700

    example file h. m

---
+++ MoleVision/example.h
@@ -0,0 +1,13 @@
+//
+//  example.h
+//  MoleVision
+//
+//  Created by Ivy Dong on 13-07-08.
+//  Copyright (c) 2013 Team Up. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface example : NSObject
+
+@end
