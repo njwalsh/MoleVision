e8851c5b0865f5ff4cb67693643b288fa73e5f1d
ivydong <idong@sfu.ca>
2013-07-29 12:13:22 -0700

    add link page and photo

---
+++ MoleVision/LinkViewController.h
@@ -0,0 +1,17 @@
+//
+//  LinkViewController.h
+//  MoleVision
+//
+//  Created by Ivy Dong on 13-07-29.
+//  Copyright (c) 2013 Team Up. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface LinkViewController : ViewController
+
+-(IBAction) skinCancerFoundation;
+-(IBAction) webMD;
+-(IBAction) canadianCancerSociety;
+
+@end
